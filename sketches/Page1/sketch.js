d35d339a6d11f4aab36ca19fcda03ec7cdc36d73
Matteo <matteo.palmas7@gmail.com>
2022-01-22 17:58:35 +0100

    Adding sketches

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -17,7 +17,7 @@ function setup(){
     //     console.log(list[i].button)
     // }
     // console.log(list)    
-    repositories=new block_for_repos(words,0,0,200,580,5,1)                                                                
+    repositories=new block_for_repos(words,0,0,200,window.height,5,1)                                                                
 }
 
 function draw(){
