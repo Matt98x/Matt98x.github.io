ee857debb79a05ea79a38e48f883dffed51dba64
Matteo <matteo.palmas7@gmail.com>
2022-01-22 17:15:24 +0100

    Adding sketches

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -1,6 +1,7 @@
 var words
 var distancing = 25
 var list = []
+var repositories
 i = 0
 function preload(){
     path = '../../repos.json'
@@ -8,20 +9,33 @@ function preload(){
 }
 
 function setup(){    
-    for(i=0;i<Object.keys(words).length;i++){
-        temp = new Button_to_repo(words[i].name,words[i].html_url,0,i*distancing,200,distancing)
-        list.push(temp)
-        list[i].button.mousePressed(list[i].callback)
+    // for(i=0;i<Object.keys(words).length;i++){
+    //     temp = new Button_to_repo(words[i].name,words[i].html_url,0,i*distancing,200,distancing)
+    //     list.push(temp)
+    //     list[i].button.mousePressed(list[i].callback)
         
-        console.log(list[i].button)
-    }
-    console.log(list)                                                                    
+    //     console.log(list[i].button)
+    // }
+    // console.log(list)    
+    repositories=new block_for_repos(words,0,0,100,700)                                                                
 }
 
 function draw(){
 
 }
 
+
+class block_for_repos{
+    constructor(Jsonclass,ULCx,ULCy,width,height){
+        len = Object.keys(Jsonclass).length
+        this.list = []
+        for(i=0;i<len;i++){
+            temp = new Button_to_repo(Jsonclass[i].name,Jsonclass[i].html_url,ULCx,ULCy+i*distancing,width,height/len)
+            this.list.push(temp)
+            this.list[i].button.mousePressed(this.list[i].callback)
+        }
+    }
+}
 
 class Button_to_repo{
     constructor(name,link,x,y,width,height){
@@ -42,11 +56,11 @@ class Button_to_repo{
     
     callback(){
         console.log(this.y)
-        for(i=0;i<list.length;i++){
+        for(i=0;i<repositories.list.length;i++){
             
-            if(list[i].x == this.x && list[i].y == this.y){
-                console.log(list[i])
-                window.open(list[i].html_url,'_self')
+            if(repositories.list[i].x == this.x && repositories.list[i].y == this.y){
+                console.log(repositories.list[i])
+                window.open(repositories.list[i].html_url,'_self')
             }
         }
     }
