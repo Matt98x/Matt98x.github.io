fb9d0952dfc9d742e4b78d1a11c7dea9908d4ff8
Matteo <matteo.palmas7@gmail.com>
2022-01-22 20:59:42 +0100

    Refreshing the commits

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -15,7 +15,7 @@ function setup(){
     //     list[i].button.mousePressed(list[i].callback)
         
     // }
-    repositories=new block_for_repos(words,0,0,200,windowHeight,5,1,"vertical")                                                                
+    repositories=new block_for_repos(words,0,0,200,windowHeight,5,1,"vertical",0)                                                                
 }
 
 function draw(){
@@ -24,27 +24,52 @@ function draw(){
 
 
 class block_for_repos{
-    constructor(Jsonclass,ULCx,ULCy,width,height,border,interspace,orientation){
+    constructor(Jsonclass,ULCx,ULCy,width,height,border,interspace,orientation, initind){
         var len = Object.keys(Jsonclass).length
         this.list = []
         canvas=createCanvas(width,height);
         background(51);
         canvas.position(ULCx,ULCy);
-        for(i=0;i<len;i++){
-            
-            var twidth = textWidth(Jsonclass[i].name)
-            var height = textHeight(Jsonclass[i].name)
-            condole.log(str(twidth)+" "+str(theight))
-            
+        encumbrance=0
+        var limit = 0
+        for(j=initind; j<len;i++){
             if(orientation=="vertical"){
-                var bwidth=width-2*border
-                var bheight=(height-len*interspace-2*border)/len
-                var spaceL=border
-                var spaceU=border+interspace+i*(height-2*border)/len
+                encumbrance+=12 + 2*interspace// this is the default
+                if(encumbrance>height-2*border){
+                    limit=j-1
+                    break
+                }
             }else{
-                var bwidth=(width-len*interspace-2*border)/len
+                encumbrance+=textWidth(Jsonclass[i].name)+2*interspace
+                if(encumbrance>width-2*border){
+                    limit=j-1
+                    break
+                }
+            }
+        }
+        for(i=initind;i<limit;i++){
+            
+            var twidth = textWidth(Jsonclass[i].name)+2*interspace
+            var theight = 12 + 2*interspace// this is the default
+            condole.log(str(twidth)+" "+str(theight))
+            band=limit-initind
+            if(orientation=="vertical"){
+                if(limit==len){
+                    var bheight = theight
+                }else{
+                    var bheight=(height-len*interspace-2*border)/band
+                }
+                var bwidth=width-2*border
+                var spaceL=border
+                var spaceU=border+interspace+i*(height-2*border)/band
+            }else{
+                if(limit==len){
+                    var bwidth = twidth
+                }else{
+                    var bwidth=(width-len*interspace-2*border)/band
+                }
                 var bheight=height-2^border
-                var spaceL=border+interspace+i*(width-2*border)/len
+                var spaceL=border+interspace+i*(width-2*border)/band
                 var spaceU=border
             }
             var temp = new Button_to_repo(Jsonclass[i].name,Jsonclass[i].html_url,ULCx+spaceL,
