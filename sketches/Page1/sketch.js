68da82c7310f4494f2cd81e16e5123b20f042fde
Matteo <matteo.palmas7@gmail.com>
2022-01-22 12:43:05 +0100

    Adding sketches

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -24,12 +24,16 @@ class Button_to_link{
         console.log('"'+str(this.html_url)+'"')     
         this.button=createButton(this.name)
         this.button.position(x,y)
-        this.button.mousePressed(function(){'"'+str(this.html_url)+'"'})
+        this.button.mousePressed(this.callback,link)
         // this.callback=function(){
         //     console.log('"'+str(this.html_url)+'"')
         //     window.open('"'+str(this.html_url)+'"')
         // }
+        
     }
-
+    callback(link){
+        console.log('"'+str(link)+'"')
+        window.open('"'+str(link)+'"')
+    }
     
 }
