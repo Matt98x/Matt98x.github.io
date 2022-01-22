aa776c98a1f6b44d11046b48feb27d5191432c41
Matteo <matteo.palmas7@gmail.com>
2022-01-23 00:41:37 +0100

    Refreshing the commits

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -13,7 +13,7 @@ function setup(){
     canvas=createCanvas(windowWidth,windowHeight);
     canvas.position(0,0)
     stringComm= "Button_to_nav"
-    nav_menu=new block_menu(words,0,0,windowWidth,nav_height,5,1,"horizontal",0,stringComm)
+    nav_menu=new block_menu(nav_json,0,0,windowWidth,nav_height,5,1,"horizontal",0,stringComm)
     stringComm= "Button_to_repo"
     repositories=new block_menu(words,0,nav_height,200,windowHeight,5,1,"vertical",0,stringComm)                                                                
 }
