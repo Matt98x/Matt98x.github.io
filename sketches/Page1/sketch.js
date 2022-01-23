609987aec8e31fa0d1a3bd25f981034e317b652f
Matteo <matteo.palmas7@gmail.com>
2022-01-23 01:10:58 +0100

    Adding sketches

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -30,10 +30,16 @@ function got_data(data){
 function setup(){  
     canvas=createCanvas(windowWidth,windowHeight);
     canvas.position(0,0)
-    stringComm= "Button_to_nav"
-    nav_menu=new block_menu(nav_json,0,0,windowWidth,nav_height,5,1,"horizontal",0,stringComm)
-    stringComm= "Button_to_repo"
-    repositories=new block_menu(words,0,nav_height,200,windowHeight,5,1,"vertical",0,stringComm)                                                                
+    while(true)
+    {
+        if(nav_json && words){
+            stringComm= "Button_to_nav"
+            nav_menu=new block_menu(nav_json,0,0,windowWidth,nav_height,5,1,"horizontal",0,stringComm)
+            stringComm= "Button_to_repo"
+            repositories=new block_menu(words,0,nav_height,200,windowHeight,5,1,"vertical",0,stringComm)                                                                
+            break
+        }
+    }
 }
 
 function draw(){
