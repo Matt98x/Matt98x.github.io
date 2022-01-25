e2ea4132604272d5e6497a8f70f99c157b21c7eb
Matt98x <matteo.palmas7@gmail.com>
2022-01-25 23:10:48 +0100

    Checking if screen update are percieved

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -82,7 +82,7 @@ function setup() {
     prevWheight = windowHeight
     prevWwidth = windowWidth
     mainscript(prevWwidth, prevWheight)
-    keyisDown(function(event) {
+    keyIsDown(function(event) {
         if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
                 event.preventDefault();
              }
