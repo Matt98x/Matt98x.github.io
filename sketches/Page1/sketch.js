997ded42f9cecf4abed5e6b2f797a6255fe43145
Matt98x <matteo.palmas7@gmail.com>
2022-01-25 19:08:51 +0100

    Deleting every element function added commit4

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -42,9 +42,6 @@ function got_data(data) {
 function mainscript() {
     while (true) {
         if (nav_json && words) {
-            canvas = createCanvas(windowWidth, windowHeight);
-            canvas.position(0, 0)
-            background(255)
             resizeCanvas(windowWidth, windowHeight)
             stringComm = "Button_to_nav"
             nav_menu = new block_menu(nav_json, 0, 0, windowWidth, nav_height * windowHeight / firstHeight, 5, 1, "horizontal", 0, stringComm)
