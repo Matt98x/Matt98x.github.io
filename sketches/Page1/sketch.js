baae2ddcf79a0064e3ece487a1b413f8c62d54c5
Matt98x <matteo.palmas7@gmail.com>
2022-01-25 23:00:25 +0100

    Checking if screen update are percieved

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -39,21 +39,7 @@ function preload() {
     prevwords = words
 
 
-    keyisDown(function(event) {
-        if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
-                event.preventDefault();
-             }
-            // 107 Num Key  +
-            // 109 Num Key  -
-            // 173 Min Key  hyphen/underscor Hey
-            // 61 Plus key  +/= key
-        })
-        
-    window.bind('mousewheel DOMMouseScroll', function (event) {
-            if (event.ctrlKey == true) {
-            event.preventDefault();
-            }
-    })
+    
 
 }
 
@@ -113,6 +99,21 @@ function setup() {
     prevWheight = windowHeight
     prevWwidth = windowWidth
     mainscript(prevWwidth, prevWheight)
+    keyisDown(function(event) {
+        if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+                event.preventDefault();
+             }
+            // 107 Num Key  +
+            // 109 Num Key  -
+            // 173 Min Key  hyphen/underscor Hey
+            // 61 Plus key  +/= key
+        })
+        
+    window.bind('mousewheel DOMMouseScroll', function (event) {
+            if (event.ctrlKey == true) {
+            event.preventDefault();
+            }
+    })
 }
 
 
