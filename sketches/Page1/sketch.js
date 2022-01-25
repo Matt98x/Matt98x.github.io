62ee34a372be44a51d5565788683faa7dbfabac7
Matt98x <matteo.palmas7@gmail.com>
2022-01-25 23:40:21 +0100

    Checking if screen update are percieved

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -82,11 +82,25 @@ function setup() {
     prevWheight = windowHeight
     prevWwidth = windowWidth
     mainscript(prevWwidth, prevWheight)
-    
+    document.body.addEventListener("wheel", zoomShortcut); //add the event
     
         
 }
 
+
+function zoomShortcut(e){
+    if(e.ctrlKey){            //[ctrl] pressed?
+      event.preventDefault();  //prevent zoom
+      if(e.deltaY<0){        //scrolling up?
+                              //do something..
+        return false;
+      }
+      if(e.deltaY>0){        //scrolling down?
+                              //do something..
+        return false;
+      }
+    }
+  }
 
 // Function in loop to handle any page modifications
 function draw() {
