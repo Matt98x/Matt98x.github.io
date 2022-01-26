07f75d109ffd6b6d1b8c0687301cd4b81d4cc0f0
Matt98x <matteo.palmas7@gmail.com>
2022-01-26 19:00:21 +0100

    Checking if screen update are percieved

--- sketches/Page1/sketch.js
+++ sketches/Page1/sketch.js
@@ -37,7 +37,6 @@ function preload() {
 
     goWiki()
     prevwords = words
-
 }
 
 
@@ -58,9 +57,9 @@ function mainscript(ww, wh) {
     while (true) {
         if (nav_json && words) {
             stringComm = "Button_to_nav"
-            nav_menu = new block_menu(nav_json, 0, 0, wwindowWidth, nav_height , 5, 1, "horizontal", 0, stringComm)
+            nav_menu = new block_menu(nav_json, 0, 0, ww, nav_height , 5, 1, "horizontal", 0, stringComm)
             stringComm = "Button_to_repo"
-            repositories = new block_menu(words, 0, nav_height , 200 , windowHeight, 5, 1, "vertical", 0, stringComm)
+            repositories = new block_menu(words, 0, nav_height , 200 , wh, 5, 1, "vertical", 0, stringComm)
             break
         }
     }
