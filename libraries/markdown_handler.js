2b1d75993f2623d9737271113fbd77b45e3c4fd2
Matt98x <matteo.palmas7@gmail.com>
2022-01-29 21:43:15 +0100

    Handling the polling

--- libraries/markdown_handler.js
+++ libraries/markdown_handler.js
@@ -1,12 +1,12 @@
 class MD_handler {
     constructor(link, oL, oU, w, h) {
-        root = createElement("div")
-        mark = createElement("zero-md")
-        mark.setAttribute("src", link)
-        root.appendChild(mark)
-        root.style.position = "absolute"
-        root.style.left = oL + "px"
-        root.style.top = oU + "px"
+        this.root = createElement("div")
+        this.mark = createElement("zero-md")
+        this.mark.setAttribute("src", this.link)
+        this.root.appendChild(this.mark)
+        this.root.style.position = "absolute"
+        this.root.style.left = oL + "px"
+        this.root.style.top = oU + "px"
         document.getElementById("body")[0].appendChild(root)
     }
 }
