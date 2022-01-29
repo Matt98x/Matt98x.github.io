ce1fe77a5dbe466f59509e6f1ad2c46466787520
Matt98x <matteo.palmas7@gmail.com>
2022-01-29 11:30:22 +0100

    Making the navigation menu an exportable library commit 3

--- sketches/Mainpage/sketch.js
+++ sketches/Mainpage/sketch.js
@@ -1,82 +1,18 @@
 
-
-var titolo;
-var r=1;
-var campagna=[];
-var k=0;
-var test;
-var data;
-
 var canvas;
 var dim=20;
 var lun=0;
 var lol;
 
 function setup() { 
+ canvas=createCanvas(windowWidth,windowHeight)
+ n_menu = new Navigation_m(windowHeight,100)
 
-  canvas=createCanvas(500,500);
-  background(51);
-  canvas.position(0,140);
-  drawData();
- 
+}
+  
+  
+  
 
- 
-  //Inizializzo la schermata
-  titolo=createElement("h1","Sito WEB");
-  titolo.position(10,0);
-
-  button1= createButton("Tasto di prova");
-  button1.mousePressed(submitWord);
-  button1.position(10,80);
-
-  test=createInput();
-  test.position(10,110);
-  
-  button2=createButton("Apri");
-  button2.position(200,110);
-  
-  // Some changes
-  
-  button2.mousePressed(printvalue);
-  
-  function printvalue() {
-    print(test.value());
-    k++;
-  }
-  //Creo una nuova campagna
-  } 
-  
-  
-  
-  function gotData(data){
-     console.log(data);
-      var keys=Object.keys(data);
-  
-    r=keys.length;
-    background(255);
-    for(var i=0;i<keys.length;i++){
-      var word=keys[i];
-      var score=data[word];
-      //var test=createP(word);
-      //test.position(10,120+i*20);
-      fill(0);
-      textSize(dim);
-      text(word,0,dim+(dim+5)*i);
-    }
-  //console.log(word);
-  }
-  
-  
-  
-  function submitWord(){
-    
-      
-    
-  }
-  
-  function drawData(){
-
-  }
 
   function draw(){ 
  
@@ -84,3 +20,56 @@ function setup() {
     
    
   }
+
+
+
+
+  // //Creo una nuova campagna
+  // } 
+  
+  
+  
+  // function gotData(data){
+  //    console.log(data);
+  //     var keys=Object.keys(data);
+  
+  //   r=keys.length;
+  //   background(255);
+  //   for(var i=0;i<keys.length;i++){
+  //     var word=keys[i];
+  //     var score=data[word];
+  //     //var test=createP(word);
+  //     //test.position(10,120+i*20);
+  //     fill(0);
+  //     textSize(dim);
+  //     text(word,0,dim+(dim+5)*i);
+  //   }
+//canvas=createCanvas(500,500);
+  // background(51);
+  // canvas.position(0,140);
+  // drawData();
+ 
+
+ 
+  // //Inizializzo la schermata
+  // titolo=createElement("h1","Sito WEB");
+  // titolo.position(10,0);
+
+  // button1= createButton("Tasto di prova");
+  // button1.mousePressed(submitWord);
+  // button1.position(10,80);
+
+  // test=createInput();
+  // test.position(10,110);
+  
+  // button2=createButton("Apri");
+  // button2.position(200,110);
+  
+  // // Some changes
+  
+  // button2.mousePressed(printvalue);
+  
+  // function printvalue() {
+  //   print(test.value());
+  //   k++;
+  // }
