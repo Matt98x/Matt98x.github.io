1bbf9f5f78441f131e112bf490c090b1dd8fb70e
Matt98x <matteo.palmas7@gmail.com>
2022-01-29 13:36:52 +0100

    Handling the polling

--- sketches/Repositories/sketch.js
+++ sketches/Repositories/sketch.js
@@ -3,11 +3,13 @@ var back_col = "#000000"
 var text_col = "#ffffff"
 var border = "0px"
 
-var words = [{ "name": "", "html_url": "","callback":function(){},"parameters":[],"side": 0,
-"border-radius": border_rad,
-"col_back": back_col,
-"col_text": text_col,
-"border": border}] // Github repositories object
+var words = [{
+    "name": "", "html_url": "", "callback": function () { }, "parameters": [], "side": 0,
+    "border-radius": border_rad,
+    "col_back": back_col,
+    "col_text": text_col,
+    "border": border
+}] // Github repositories object
 var repositories // repositories menu object
 var nav_menu // navigation menu object
 var left_menu // left menu
@@ -16,7 +18,9 @@ var prevWwidth // previous Window width (to check for changes)
 var prevWheight // previous window height (to check for changes)
 var repowidth = 200 // width of the repository list
 var prevtime
-var reloadCount
+var reloadCount // how many times the page has been reloaded
+var last_reset // the last polling reset obtained
+var remaining // how many pollings I have left
 var expasionCoeff = 1
 var wcopy = words
 var n_menu
@@ -26,48 +30,62 @@ var header
 
 // Preload function to get any stored data
 function preload() {
+
     goWiki()
+
+
+
     var state = history.state || {}
     reloadCount = state.reloadCount || 0
-    if (performance.navigation.type == 1) {
-        state.reloadCount = ++reloadCount
-        history.replaceState(state, null, document.URL)
-    } else if (reloadCount) {
-        delete state.reloadCount
-        reloadCount = 0
-        history.replaceState(state, null, document.URL)
-    }
+
+
     prevwords = words
 }
 
 // Function to get the list of github repositories
 function goWiki() {
-    path = 'https://api.github.com/users/Matt98x/repos'
-    loadJSON(path, got_data, 'jsonp')
+    path = "test.json"
+    loadJSON(path, check_polling)
+    var current_time = new Date.getTime() / 1000
+    if (remaining > 0 || current_time > last_reset) {
+        path = 'https://api.github.com/users/Matt98x/repos'
+        loadJSON(path, got_data, 'jsonp')
+    }
+}
+
+function check_polling(data) {
+    last_reset = data.last_reset
+    remaining = data.remaining
+
 }
 
 // Callback to store the list of repositories
 function got_data(data) {
-    header = data.header
+    header = data.meta
     console.log(data)
     words = data.data
     let len = Object.keys(words).length
     for (let i = 0; i < len; i++) {
         words[i].callback = page_refer
         words[i].parameters = words[i].html_url
-        words[i].side=0
+        words[i].side = 0
         words[i].border_radius = border_rad
-        words[i].col_back= back_col
-        words[i].col_text= text_col
+        words[i].col_back = back_col
+        words[i].col_text = text_col
         words[i].border = border
     }
+    toTextMessage={
+        "remaining":header["X-RateLimit-Reset"],
+        "last_reset":header["X-RateLimit-Remaining"]
+    }
+    saveJson(toTextMessage,"test.json")
 }
 
 // Main script of the page
 function mainscript(ww, wh) {
-            n_menu = new Navigation_m(ww,100)
-            left_menu = new block_menu(wcopy, ww - repowidth, n_menu.nav_height, repowidth, wh - n_menu.nav_height, 5, 1, "vertical", 0)
-            repositories = new block_menu(words, 0, n_menu.nav_height, repowidth, wh, 5, 1, "vertical", 0)
+    n_menu = new Navigation_m(ww, 100)
+    left_menu = new block_menu(wcopy, ww - repowidth, n_menu.nav_height, repowidth, wh - n_menu.nav_height, 5, 1, "vertical", 0)
+    repositories = new block_menu(words, 0, n_menu.nav_height, repowidth, wh, 5, 1, "vertical", 0)
 }
 
 // Function to delete every html elements
