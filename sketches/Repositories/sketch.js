0398e9fc01a229b27aafcda77f01d3d053c4d1be
Matt98x <matteo.palmas7@gmail.com>
2022-01-29 14:36:33 +0100

    Handling the polling

--- sketches/Repositories/sketch.js
+++ sketches/Repositories/sketch.js
@@ -52,6 +52,7 @@ function goWiki() {
     remaining = data.remaining
     current_time = new Date()
     current_time = current_time.getTime() / 1000
+    console.log(Object.keys(data).length)
     console.log(data["remaining"])
     console.log(data["remaining"] > 0)
     if (data["remaining"] > 0 || current_time > data["last_reset"]) {
