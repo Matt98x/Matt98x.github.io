991ca406a2fdd9753b5841171539e92af672224f
Matt98x <matteo.palmas7@gmail.com>
2022-01-30 18:55:35 +0100

    add div to the block_menu

--- libraries/block_lib.js
+++ libraries/block_lib.js
@@ -7,7 +7,7 @@ class block_menu {
         this.width = width
         this.height = height
         this.graphics = createGraphics(width, height)
-        this.graphics.position(500,500)
+        this.graphics.position(0,0)
         this.graphics.background(51)
         this.root.elt.style.position = "absolute"
         this.root.elt.style.left = ULCx + "px"
