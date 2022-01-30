3ff47d1e9573bc65427b0634b753f6feaa678cc0
Matt98x <matteo.palmas7@gmail.com>
2022-01-30 19:15:47 +0100

    add div to the block_menu

--- libraries/block_lib.js
+++ libraries/block_lib.js
@@ -11,8 +11,8 @@ class block_menu {
         this.root.elt.style.position = "absolute"
         this.root.elt.style.left = ULCx + "px"
         this.root.elt.style.top = ULCy + "px"
-        this.root.elt.style.width = str(width + border) +"px"
-        this.root.elt.style.height = str(height + border) +"px"
+        this.root.elt.style.width = str(width + 2*border) +"px"
+        this.root.elt.style.height = str(height + 2*border) +"px"
         this.root.elt.style.overflow = "scroll"
         console.log(this.graphics)
         this.root.elt.appendChild(this.graphics.elt)
